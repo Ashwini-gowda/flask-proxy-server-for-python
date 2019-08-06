6832c41e0d261f17905c07fab2512e6bfe20fa3a
Shivani <shivani.raj@nighthack.in>
2019-08-06 16:37:14 +0530

    jsonresponse

--- app1.py
+++ app1.py
@@ -1,16 +1,58 @@
-from flask import Flask 
-from flask import Flask, render_template
-app = Flask(__name__)     
+from flask import Flask, render_template, request, jsonify
+from flask_mysqldb import MySQL
+app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
-@app.route('/hi')
-def main():
-    return render_template('hello.html')
-      
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'mypgm_db'
 
-if __name__ == '__main__':  
-    app.run(debug=True)
+mysql = MySQL(app)
+
+@app.route('/form-example', methods=['GET','POST'])
+def form_example():
+    if request.method == 'POST': 
+	id = request.form['id']
+        language = request.form['language']
+        framework = request.form['framework']
+	cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO mypgm(id, language, framework) VALUES (%s, %s, %s)", (id, language, framework))
+        mysql.connection.commit()
+	
+	cur.execute("SELECT * FROM mypgm")
+	rows = cur.fetchall()
+	resp = jsonify(rows)
+	resp.status_code = 200
+	cur.close()
+	return resp
+        
+       
+
+    return '''<form method="POST">
+		  Id: <input type="text" name="id"><br>
+                  Language: <input type="text" name="language"><br>
+                  Framework: <input type="text" name="framework"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>''' 
+
+@app.route('/form-process', methods=['GET','POST'])
+def form_process():
+    if request.method == 'POST':
+	id = request.form['id'] 
+        username = request.form['username']
+        emailid = request.form['emailid']
+	password = request.form['password']
 
 
+        return jsonify(id=id, username=username, emailid=emailid, password=password)
 
+    return '''<form method="POST">
+		  Id: <input type="text" name="id"><br>
+                  UserName: <input type="text" name="username"><br>
+                  Emailid: <input type="text" name="emailid"><br>
+		  password: <input type="password" name="password"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>''' 
+ 
+if __name__ == "__main__":
+    app.run(debug='true')
